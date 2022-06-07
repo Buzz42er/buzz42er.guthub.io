a2a5a699b6580fa45acedf43814be5091f8c90a2
Buzz <jure.eres42@gmail.com>
2022-06-07 22:19:52 +0200

    0.4.6

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ var imgCoreToolsArray = [
     './slike-projekata/Snimka zaslona (16).png',
     './slike-projekata/Snimka zaslona (11).png',
     './slike-projekata/Snimka zaslona (8).png',
-    './slike-projekata/Snimka zaslona (20).png',
+    './slike-projekata/Snimka zaslona (20).png'
 
 ]
 
@@ -26,20 +26,22 @@ var imgReactArray = [
 ]
 
 var imgAngularArray = [
-    './slike-projekata/Snimka zaslona (15).png',
-    './slike-projekata/Snimka zaslona (15).png',
     './slike-projekata/Snimka zaslona (15).png'
+    
 
 ]
 
 var imgRubyjQueryArray = [
-    './slike-projekata/Snimka zaslona (15).png',
-    './slike-projekata/Snimka zaslona (15).png',
     './slike-projekata/Snimka zaslona (15).png'
 
 ]
 //number for compare place of item in array
-var numCT, numD, numS, numR, numA, numQ = 0
+var numCT = 0,
+    numD = 0,  
+    numS = 0, 
+    numR = 0, 
+    numA = 0, 
+    numQ = 0
 
 
 //function for button before
@@ -71,4 +73,117 @@ function afterCT() {
     }
     //exchange imgSlider sorce with img array's item with same value as num
     imgCoreTools.src = imgCoreToolsArray[numCT]
+}
+
+
+
+function beforeD() {
+    var imgDesign = document.getElementById('imgDesign')
+    numD--
+
+    if (numD < 0) {
+        numD = imgDesignArray.length-1
+    }
+    imgDesign.src = imgDesignArray[numD]
+    console.log(imgDesignArray[numD])
+}
+
+//create array of pictures that you wanna slide
+//create number var that you can compare with position of item in array
+
+//create a function for onclick event
+//put img that you wanna slide inside of a var
+//subtract 1 from num var
+//if num is less then 0
+//make num equle to array lenght
+//replace img src with array item that has same place value as num
+
+function afterD() {
+    var imgDesign = document.getElementById('imgDesign')
+    numD++
+    
+    if (numD >= imgDesignArray.length) {
+        numD = 0
+    }
+    imgDesign.src = imgDesignArray[numD]
+}
+
+function beforeS() {
+    var imgSupport = document.getElementById('imgSupport')
+    numS--
+
+    if (numS < 0) {
+        numS = imgSupportArray.length-1
+    }
+    imgSupport.src = imgSupportArray[numS]
+}
+
+function afterS() {
+    var imgSupport = document.getElementById('imgSupport')
+    numS++
+    
+    if (numS >= imgSupportArray.length) {
+        numS = 0
+    }
+    imgSupport.src = imgSupportArray[numS]
+}
+
+function beforeR() {
+    var imgReact = document.getElementById('imgReact')
+    numR--
+
+    if (numR < 0) {
+        numR = imgReactArray.length-1
+    }
+    imgReact.src = imgReactArray[numR]
+}
+
+function afterR() {
+    var imgReact = document.getElementById('imgReact')
+    numR++
+
+    if (numR >= imgReactArray.length) {
+        numR = 0
+    }
+    imgReact.src = imgReactArray[numR]
+}
+
+function beforeA() {
+    var imgAngular = document.getElementById('imgAngular')
+    numA--
+
+    if (numA < 0) {
+        numA = imgAngularArray.length-1
+    }
+    imgAngular.src = imgAngularArray[numA]
+}
+
+function afterA() {
+    var imgAngular = document.getElementById('imgAngular')
+    numA++
+
+    if (numA >= imgAngularArray.length) {
+        numA = 0
+    }
+    imgAngular.src = imgAngularArray[numA]
+}
+
+function beforeQ() {
+    var imgRubyjQuery = document.getElementById('imgRubyjQuery')
+    numQ--
+
+    if (numQ < 0) {
+        numQ = imgRubyjQueryArray.length-1
+    }
+    imgRubyjQuery.src = imgRubyjQueryArray[numQ]
+}
+
+function afterQ() {
+    var imgRubyjQuery = document.getElementById('imgRubyjQuery')
+    numQ++
+
+    if (numQ >= imgRubyjQueryArray.length) {
+        numQ = 0
+    }
+    imgRubyjQuery.src = imgRubyjQueryArray[numQ]
 }
